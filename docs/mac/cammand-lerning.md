9d437ba9df75c006c93ab1e109d7345df22905f1
Roy <iRoySwift@iCloud.com>
2023-06-06 16:14:16 +0800

    ✨ git tutorial

---
+++ docs/mac/cammand-lerning.md
@@ -0,0 +1,9 @@
+---
+id: mac-find
+# sidebar_position: 1
+title: Mac Find 文件
+---
+
+# find
+
+find / -iname "java"
--- sidebars/tools.js
+++ sidebars/tools.js
@@ -7,54 +7,54 @@
  */
 
 const tools = {
-  type: 'category',
-  label: '工具',
-  collapsed: false,
-  items: [
-    {
-      type: 'category',
-      label: 'Git',
-      link: {
-        type: 'generated-index',
-        description: "Let's learn Git!",
-        keywords: ['git']
-      },
-      collapsed: false,
-      items: ['tools/git/gitmoji']
-    },
-    {
-      type: 'category',
-      label: 'Lerna',
-      link: {
-        type: 'generated-index',
-        description: "Let's learn Lerna!",
-        keywords: ['Lerna']
-      },
-      collapsed: false,
-      items: ['tools/lerna/learn-lerna']
-    },
-    {
-      type: 'category',
-      label: 'NPM',
-      link: {
-        type: 'generated-index',
-        description: "Let's learn NPM!",
-        keywords: ['NPM']
-      },
-      collapsed: false,
-      items: ['tools/npm/learn-npm']
-    },
-    {
-      type: 'category',
-      label: '开发工具',
-      link: {
-        type: 'generated-index',
-        description: "Let's learn NPM!",
-        keywords: ['idea', 'vscode']
-      },
-      collapsed: false,
-      items: ['tools/editor/idea&vscode']
-    }
-  ]
+    type: 'category',
+    label: '工具',
+    collapsed: false,
+    items: [
+        {
+            type: 'category',
+            label: 'Git',
+            link: {
+                type: 'generated-index',
+                description: "Let's learn Git!",
+                keywords: ['git']
+            },
+            collapsed: false,
+            items: ['tools/git/git-tutorial', 'tools/git/gitmoji']
+        },
+        {
+            type: 'category',
+            label: 'Lerna',
+            link: {
+                type: 'generated-index',
+                description: "Let's learn Lerna!",
+                keywords: ['Lerna']
+            },
+            collapsed: false,
+            items: ['tools/lerna/learn-lerna']
+        },
+        {
+            type: 'category',
+            label: 'NPM',
+            link: {
+                type: 'generated-index',
+                description: "Let's learn NPM!",
+                keywords: ['NPM']
+            },
+            collapsed: false,
+            items: ['tools/npm/learn-npm']
+        },
+        {
+            type: 'category',
+            label: '开发工具',
+            link: {
+                type: 'generated-index',
+                description: "Let's learn NPM!",
+                keywords: ['idea', 'vscode']
+            },
+            collapsed: false,
+            items: ['tools/editor/idea&vscode']
+        }
+    ]
 };
 module.exports = tools;
